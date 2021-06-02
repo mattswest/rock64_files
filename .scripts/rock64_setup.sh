ed4bc4dd9971b1826f0005c0930ed21b2a8c3028
mattswest <mattswest@gmail.com>
2021-06-01 20:12:11 -0400

    readability

--- .scripts/rock64_setup.sh
+++ .scripts/rock64_setup.sh
@@ -1,3 +1,11 @@
 !/bin/bash
 
-sudo useradd matt && sudo passwd matt && sudo usermod -a -G sudo matt && sudo mkdir /home/matt && sudo chown -R matt:matt /home/matt && sudo chsh matt && sudo scp matt@192.168.86.190:/home/matt/.bashrc /home/matt && sudo vi /etc/hostname && sudo vi /etc/hosts && sudo apt update && sudo apt install snapd && sudo shutdown now
+sudo useradd matt \
+&& sudo passwd matt \
+&& sudo usermod -a -G sudo matt \
+& sudo mkdir /home/matt \
+&& sudo chown -R matt:matt /home/matt \
+&& sudo chsh matt \
+&& sudo scp matt@192.168.86.190:/home/matt/.bashrc /home/matt \
+&& sudo vi /etc/hostname \
+&& sudo vi /etc/hosts
