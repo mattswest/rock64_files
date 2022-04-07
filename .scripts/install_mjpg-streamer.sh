f7af86f01b49bde0f5c9dc400599d56338cd7a84
mattswest <mattswest@gmail.com>
2022-04-07 15:08:31 -0400

    Update install_mjpg-streamer.sh

--- .scripts/install_mjpg-streamer.sh
+++ .scripts/install_mjpg-streamer.sh
@@ -4,7 +4,7 @@ apt-get install cmake libjpeg8-dev
 apt-get install gcc g++
 
 cd /tmp
-git clone https://github.com/jacksonliam/mjpg-streamer.git
+git clone https://github.com/mattswest/mjpg-streamer
 cd mjpg-streamer/mjpg-streamer-experimental
 
 make
