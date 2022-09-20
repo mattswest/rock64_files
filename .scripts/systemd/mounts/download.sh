47e17d1f6e0c25d9b7daf34c3769ae34fdd79543
mattswest <mattswest@gmail.com>
2022-09-20 19:49:39 -0400

    removed 'sudo's, added comments

--- .scripts/systemd/mounts/download.sh
+++ .scripts/systemd/mounts/download.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+#Run as root
+
+#Change directory for downloads
 cd /etc/systemd/system
 
-sudo wget https://github.com/mattswest/rock64_files/raw/main/.scripts/systemd/mounts/data.mount
-sudo wget https://github.com/mattswest/rock64_files/raw/main/.scripts/systemd/mounts/storage.mount
-sudo wget https://github.com/mattswest/rock64_files/raw/main/.scripts/systemd/mounts/mnt-sdb1-storage-audio.mount
-sudo wget https://github.com/mattswest/rock64_files/raw/main/.scripts/systemd/mounts/mnt-sdf1-movies.mount
+#Download service files
+wget https://github.com/mattswest/rock64_files/raw/main/.scripts/systemd/mounts/storage.mount
+wget https://github.com/mattswest/rock64_files/raw/main/.scripts/systemd/mounts/mnt-sdb1-storage-audio.mount
+wget https://github.com/mattswest/rock64_files/raw/main/.scripts/systemd/mounts/mnt-sdf1-movies.mount
 
-sudo systemctl enable /etc/systemd/system/data.mount
-sudo systemctl enable /etc/systemd/system/storage.mount
-sudo systemctl enable /etc/systemd/system/mnt-sdb1-storage-audio.mount
-sudo systemctl enable /etc/systemd/system/mnt-sdf1-movies.mount
+#Enable service files
+systemctl enable /etc/systemd/system/data.mount
+systemctl enable /etc/systemd/system/storage.mount
+systemctl enable /etc/systemd/system/mnt-sdb1-storage-audio.mount
+systemctl enable /etc/systemd/system/mnt-sdf1-movies.mount
 
