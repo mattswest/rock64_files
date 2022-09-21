0773630ef037fe6c78d05138d3240861f0975677
mattswest <mattswest@gmail.com>
2022-09-20 20:12:38 -0400

    Update download.sh

--- .scripts/systemd/mounts/download.sh
+++ .scripts/systemd/mounts/download.sh
@@ -5,7 +5,14 @@
 #Change directory for downloads
 cd /etc/systemd/system
 
+#Remove existing files
+rm data.mount
+rm storage.mount
+rm mnt-sdb1-storage-audio.mount
+rm mnt-sdf1-movies.mount
+
 #Download service files
+wget https://github.com/mattswest/rock64_files/raw/main/.scripts/systemd/mounts/data.mount
 wget https://github.com/mattswest/rock64_files/raw/main/.scripts/systemd/mounts/storage.mount
 wget https://github.com/mattswest/rock64_files/raw/main/.scripts/systemd/mounts/mnt-sdb1-storage-audio.mount
 wget https://github.com/mattswest/rock64_files/raw/main/.scripts/systemd/mounts/mnt-sdf1-movies.mount
