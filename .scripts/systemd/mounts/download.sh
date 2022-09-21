72cd5be4fbacbd1a19a4fd61a32a64425e5fc513
mattswest <mattswest@gmail.com>
2022-09-20 22:38:06 -0400

    Update download.sh

--- .scripts/systemd/mounts/download.sh
+++ .scripts/systemd/mounts/download.sh
@@ -6,18 +6,21 @@
 cd /etc/systemd/system
 
 #Remove existing files
+echo "Removing existing .mount files"
 rm data.mount*
 rm storage.mount*
 rm mnt-sdb1-storage-audio.mount*
 rm mnt-sdf1-movies.mount*
 
 #Download service files
-wget https://github.com/mattswest/rock64_files/raw/main/.scripts/systemd/mounts/data.mount
-wget https://github.com/mattswest/rock64_files/raw/main/.scripts/systemd/mounts/storage.mount
-wget https://github.com/mattswest/rock64_files/raw/main/.scripts/systemd/mounts/mnt-sdb1-storage-audio.mount
-wget https://github.com/mattswest/rock64_files/raw/main/.scripts/systemd/mounts/mnt-sdf1-movies.mount
+echo "Downloading .mount files"
+wget -q https://github.com/mattswest/rock64_files/raw/main/.scripts/systemd/mounts/data.mount && echo "Downloaded data.mount" 
+wget -q https://github.com/mattswest/rock64_files/raw/main/.scripts/systemd/mounts/storage.mount && echo "Downloaded storage.mount"
+wget -q https://github.com/mattswest/rock64_files/raw/main/.scripts/systemd/mounts/mnt-sdb1-storage-audio.mount && echo "Downloaded mnt-sdb1-storage-audio.mount"
+wget -q https://github.com/mattswest/rock64_files/raw/main/.scripts/systemd/mounts/mnt-sdf1-movies.mount && echo "Downloaded mnt-sdf1-movies.mount"
 
 #Enable service files
+echo "Enabling .mount files"
 systemctl enable /etc/systemd/system/data.mount
 systemctl enable /etc/systemd/system/storage.mount
 systemctl enable /etc/systemd/system/mnt-sdb1-storage-audio.mount
