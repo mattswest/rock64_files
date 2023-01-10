94f193a9243ff3f19db21133ca208a3067709c64
matt <mattswest@gmail.com>
2023-01-10 06:36:45 -0500

    working solo python script

--- .scripts/lcd/lcd_display.py
+++ .scripts/lcd/lcd_display.py
@@ -2,7 +2,7 @@
 
 import subprocess
 import time
-import socket
+import netifaces
 import json
 
 
@@ -15,15 +15,15 @@ lcd = CharLCD(i2c_expander='PCF8574', address=0x3f, port=1,
               backlight_enabled=True)
 
 # Get IP address
-host_name = socket.gethostname()
-host_ip = socket.gethostbyname(host_name)
+ip_address = netifaces.ifaddresses('eth0')[netifaces.AF_INET][0]['addr']
 
 # Write IP address to LCD
-lcd.write_string(host_ip)
+lcd.write_string(ip_address)
 lcd.cursor_pos = (1,0)
 
 # Get time remaining from OctoPrint
 octoprint_output = subprocess.run(["/home/matt/OctoPrint/bin/octoprint", "client", "get", "/api/job"], stdout=subprocess.PIPE).stdout.decode("utf-8")
+octoprint_output = '\n'.join(octoprint_output.split('\n')[1:])
 
 # parse the json output
 octoprint_output_dict = json.loads(octoprint_output)
@@ -36,7 +36,7 @@ minutes = time_remaining // 60
 seconds = time_remaining % 60
 
 # Write time remaining to LCD
-lcd.write_string("{minutes}m {seconds}s left")
+lcd.write_string(f"{minutes}m {seconds}s left")
 
 # Sleep for 10 seconds
 time.sleep(10)
